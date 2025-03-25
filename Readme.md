7cfb82dc70609d62e3b7c99c43113426ba5ebdf8
Tech-master1234 <manojbarathi1234@gmail.com>
2025-03-25 12:17:10 +0530

    hello world

--- Readme.md
+++ Readme.md
@@ -1,1 +1,7 @@
 This repo contains js
+
+node -v
+
+
+To install sqlite
+npm install sqlite3
--- helloworld.js
+++ helloworld.js
@@ -1,41 +1,6 @@
-/*
-// This is a single-line comment
-console.log("Hello, World!");
+var http = require ("http");
 
-// This is a comment
-let name = "John"; // A string
-let age = 25; // A number
-console.log(name, age); // Prints the values
-
-
-let age = 10;
-
-if (age > 18) {
-    console.log("You're an adult!");
-} else {
-    console.log("You're not an adult.");
-}
-
-
-
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-console.log(greet("Alice")); // Calls the function
-
-
-
-
-document.getElementById("myButton").addEventListener("click", function() {
-    alert("Button clicked!");
-});
-let person = { name: "John", age: 30 };
-let hobbies = ["Reading", "Gaming", "Traveling"];
-console.log(person.name); // Accessing object property
-console.log(hobbies[0]); // Accessing array element
-*/
-
-
-setTimeout(() => {
-    console.log("Hello after 2 seconds!");
-}, 2000);
+http.createServer(function (req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello world');
+}).listen(8080);
