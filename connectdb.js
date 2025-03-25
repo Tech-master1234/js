fa322652fe61db7641037564aa35d69bb9e4cfe3
Tech-master1234 <manojbarathi1234@gmail.com>
2025-03-25 12:48:46 +0530

    DB insert

--- connectdb.js
+++ connectdb.js
@@ -1,1 +1,8 @@
-const sqlite3 = require(sqlite3);
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./test.db');
+db.run('INSERT INTO workshop(Name), VALUES(?)',
+    ['SINCET-AIDS'],
+    function(error){
+        console.log("Inserted !");
+    }
+);
